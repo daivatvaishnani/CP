e92b83eb36c84d949c3a70238ce61e41d19fa50b
Daivat Vaishnani <daivatvaishnani18@gmail.com>
2021-08-01 01:49:49 +0530

    add more code

---
+++ java/FastReader.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.StringTokenizer;
+
+public class FastReader extends BufferedReader {
+    StringTokenizer st;
+
+    public FastReader(Reader in, int sz) {
+        super(in, sz);
+    }
+
+    public FastReader(Reader in) {
+        super(in);
+    }
+
+    private String next() throws IOException {
+        while (st == null || !st.hasMoreElements()) {
+            st = new StringTokenizer(readLine());
+        }
+        return st.nextToken();
+    }
+
+    int nextInt() throws IOException {
+        return Integer.parseInt(next());
+    }
+
+    long nextLong() throws IOException {
+        return Long.parseLong(next());
+    }
+
+    double nextDouble() throws IOException {
+        return Double.parseDouble(next());
+    }
+
+    String nextLine() throws IOException {
+        return readLine();
+    }
+}
---
+++ java/Logger.java
@@ -0,0 +1,10 @@
+public class Logger {
+    static void log(Object message) {
+        System.out.println(message);
+    }
+
+    static void log(String format, Object... args) {
+        String message = String.format(format, args);
+        System.out.println(message);
+    }
+}
