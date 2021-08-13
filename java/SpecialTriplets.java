20196dc8fcdc984aa07ab1c2873d6c2cffd2f5d1
Daivat Vaishnani <daivatvaishnani18@gmail.com>
2021-08-13 23:21:16 +0530

    add codes

---
+++ java/SpecialTriplets.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class SpecialTriplets {
+	private static long solve(int n) {
+		long ans = 0;
+		if (n <= 1) {
+			return 0;
+		}
+		for (int c = 1; c <= n; ++c) {
+			for (int b = 2*c; b <= n; b += c) {
+				ans += Math.floor((n - c + 0.0) / b) + 1;
+			}
+		}
+		return ans;
+	}
+
+	public static void main(String[] args) throws IOException {
+		FastReader fastReader = new FastReader(new InputStreamReader(System.in));
+		int cases = fastReader.nextInt();
+		while (cases-- > 0) {
+			int n = fastReader.nextInt();
+			System.out.println(solve(n));
+		}
+	}
+}
